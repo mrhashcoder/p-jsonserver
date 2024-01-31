631fc95b62d76ad1871f5676a60d014b68381969
Abhishek Sharma <mrhashcoder@Abhisheks-MBP.bbrouter>
2024-01-31 18:58:33 +0530

    uploading .env

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const SUPABASE_URL = process.env.SUPABASE_URL
 const SUPABASE_KEY = process.env.SUPABASE_KEY
 const PHOTO_ALBUM_KEY = process.env.PHOTO_ALBUM_KEY
@@ -11,5 +11,5 @@ module.exports = {
     PORT,
     SUPABASE_URL,
     SUPABASE_KEY,
-    PHOTO_ALBUM_KEY
+    PHOTO_ALBUM_KEY,
 }
